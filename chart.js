99fc1244cc336f33fe44d77aa34ba9b928dc6462
CristianGrigorita <grigoria.c@gmail.com>
2018-04-21 21:52:38 +0300

    new graph set up and tsv file

--- chart.js
+++ chart.js
@@ -15,12 +15,13 @@ var chartData = [];
 				amptitude: amptitude
 			});
 		}
-		var chart = AmCharts.makeChart("chartdiv",{
+		var chart = AmCharts.makeChart("sacade",{
 			"type": "serial",
 	  		"theme" : "light",
 	  		"mouseWheelZoomEnabled":true,
 			"legend": {
-	  			"useGraphSettings": true
+	  			"useGraphSettings": true,
+	  			"position" : "right",
 	  		},
 	  		"dataProvider": chartData,
 	  		"synchronizeGrid":true,
@@ -104,7 +105,201 @@ var chartData = [];
 	
 	
    	console.log("chart", arrayForGraph);
-	
-		
-	return chartData;
 	}
+
+
+function generateFixedChartData(arrayFixationPointX){
+var chartData = [];
+		var benchAmptitude = 0;
+		var amptitude = 0;
+		var time = 0;
+
+		for (var i = 0; i < arrayFixationPointX.length; i++){
+			benchAmptitude =  arrayFixationPointX[i][1];
+			amptitude = arrayFixationPointX[i][2];
+			time = arrayFixationPointX[i][0]
+
+			chartData.push({
+				time: time,
+				benchAmptitude: benchAmptitude,
+				amptitude: amptitude
+			});
+		}
+		var chart = AmCharts.makeChart("fixPoint",{
+			"type": "serial",
+	  		"theme" : "light",
+	  		"mouseWheelZoomEnabled":true,
+			"legend": {
+	  			"useGraphSettings": true,
+	  			"position" : "right",
+	  		},
+	  		"dataProvider": chartData,
+	  		"synchronizeGrid":true,
+	  		"valueAxes": [{
+	    		'id':'v1',
+	    		'axisColor':'#FF6600',
+	    		'axisThickness': 2,
+	    		'axisAlpha': 1,
+	    		'position': 'left'
+	    	}, {
+	 			"id":"v2",
+	        	"axisColor": "#0000ff",
+	        	"axisThickness": 2,
+	        	"axisAlpha": 1,
+	        	"position": "right"
+	    	}],
+	    	"graphs": [{
+		    	"valueAxis": "v1",
+		        "lineColor": "#FF6600",
+		        "bullet": "round",
+		        "bulletBorderThickness": 2,
+		        "hideBulletsCount": 30,
+		        "title": "benchmark",
+		        "valueField": "benchAmptitude",
+				"fillAlphas": 0
+	    	},
+	    	{
+		    	"valueAxis": "v2",
+		        "lineColor": "#0000ff",
+		        "bullet": "square",
+		        "bulletBorderThickness": 2,
+		        "hideBulletsCount": 30,
+		        "title": "patient",
+		        "valueField": "amptitude",
+				"fillAlphas": 0	
+	    	}],
+	    	"chartScrollbar": {},
+			"chartCursor": {
+			   	"pan": true,
+			    "valueLineEnabled": true,
+			    "valueLineBalloonEnabled": true,
+			    "cursorAlpha":1,
+			    "cursorColor":"#258cbb",
+			    "valueLineAlpha":0.2,
+			   	"valueZoomable":true
+			},
+			"categoryField": "time",
+			"categoryAxis": {
+	   			"gridPosition": "start",
+	  		},
+	  		"export": {
+	    		"enabled" : true,
+	    		"postion" : "bottom-right"
+			},
+			"marginRight": 40,
+			"marginLeft": 40,
+			"autoMarginOffset": 20,
+		   	"balloon": {
+		  	     "borderThickness": 1,
+		   	     "shadowAlpha": 0
+		   	 },
+		    "titles": [{
+	     	 		"size": 15,
+	     	 		"text": "Fixation Point"
+	     	 	}],
+		    "valueScrollbar":{
+		      "oppositeAxis":true,
+		      "offset":50,
+		      "scrollbarHeight":10
+		    },
+
+	});
+}
+
+function generateGazePoint(arrayGazePointX){
+var chartData = [];
+		var benchAmptitude = 0;
+		var amptitude = 0;
+		var time = 0;
+
+		for (var i = 0; i < arrayFixationPointX.length; i++){
+			benchAmptitude =  arrayFixationPointX[i][1];
+			amptitude = arrayFixationPointX[i][2];
+			time = arrayFixationPointX[i][0]
+
+			chartData.push({
+				time: time,
+				benchAmptitude: benchAmptitude,
+				amptitude: amptitude
+			});
+		}
+		var chart = AmCharts.makeChart("fixPoint",{
+			"type": "serial",
+	  		"theme" : "light",
+	  		"mouseWheelZoomEnabled":true,
+			"legend": {
+	  			"useGraphSettings": true,
+	  			"position" : "right",
+	  		},
+	  		"dataProvider": chartData,
+	  		"synchronizeGrid":true,
+	  		"valueAxes": [{
+	    		'id':'v1',
+	    		'axisColor':'#FF6600',
+	    		'axisThickness': 2,
+	    		'axisAlpha': 1,
+	    		'position': 'left'
+	    	}, {
+	 			"id":"v2",
+	        	"axisColor": "#0000ff",
+	        	"axisThickness": 2,
+	        	"axisAlpha": 1,
+	        	"position": "right"
+	    	}],
+	    	"graphs": [{
+		    	"valueAxis": "v1",
+		        "lineColor": "#FF6600",
+		        "bullet": "round",
+		        "bulletBorderThickness": 2,
+		        "hideBulletsCount": 30,
+		        "title": "benchmark",
+		        "valueField": "benchAmptitude",
+				"fillAlphas": 0
+	    	},
+	    	{
+		    	"valueAxis": "v2",
+		        "lineColor": "#0000ff",
+		        "bullet": "square",
+		        "bulletBorderThickness": 2,
+		        "hideBulletsCount": 30,
+		        "title": "patient",
+		        "valueField": "amptitude",
+				"fillAlphas": 0	
+	    	}],
+	    	"chartScrollbar": {},
+			"chartCursor": {
+			   	"pan": true,
+			    "valueLineEnabled": true,
+			    "valueLineBalloonEnabled": true,
+			    "cursorAlpha":1,
+			    "cursorColor":"#258cbb",
+			    "valueLineAlpha":0.2,
+			   	"valueZoomable":true
+			},
+			"categoryField": "time",
+			"categoryAxis": {
+	   			"gridPosition": "start",
+	  		},
+	  		"export": {
+	    		"enabled" : true,
+	    		"postion" : "bottom-right"
+			},
+			"marginRight": 40,
+			"marginLeft": 40,
+			"autoMarginOffset": 20,
+		   	"balloon": {
+		  	     "borderThickness": 1,
+		   	     "shadowAlpha": 0
+		   	 },
+		    "titles": [{
+	     	 		"size": 15,
+	     	 		"text": "Fixation Point"
+	     	 	}],
+		    "valueScrollbar":{
+		      "oppositeAxis":true,
+		      "offset":50,
+		      "scrollbarHeight":10
+		    },
+
+	});
+}
